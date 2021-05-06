fe9f5d7ac3608e0f90ca8ba07cecae5eb5f3945e
Nur Gür <75504274+nurrgur@users.noreply.github.com>
2021-05-06 23:28:09 +0300

    Yildiz kullanarak ici bos kare olusturma

---
+++ YıldızÖrnek/YıldızÖrnek/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace YıldızOrnek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Oluşturmak istediğiniz karenin kenar uzunluğunu giriniz..");
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= k; i++) 
+            {
+
+                if (i==1 || i == k)
+                {
+                    for(int x = 1; x <= k; x++)
+                    {
+                        Console.Write("* ");
+                    }
+                    Console.WriteLine();
+                }
+                else
+                {
+                    for(int y = 1; y <= k; y++)
+                    {
+                        if (y == 1 || y == k)
+                        {
+                            Console.Write("* ");
+                            if (y == k) Console.WriteLine();
+                        }
+                        else Console.Write("  ");
+                    }
+                }
+            }
+            Console.Read();
+        }
+    }
+}
+
+
